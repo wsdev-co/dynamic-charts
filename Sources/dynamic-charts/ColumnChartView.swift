218c041a5bef019ce2c886a6020f3c7b80cc79da
Inagamjanov <umarjoninogomjonov9@gmail.com>
2023-01-18 23:06:40 +0500

    wsu

--- Sources/dynamic-charts/ColumnChartView.swift
+++ Sources/dynamic-charts/ColumnChartView.swift
@@ -113,18 +113,76 @@ public struct ColumnChartView: View {
                 }
                 
                 // MARK: Bar Graph
-                BarGraph(graph_data: chart_data,
-                         x_axis_color: x_axis_color,
-                         x_name_color: x_name_color,
-                         graph_gradient: chart_gradient,
-                         is_selectable: is_selectable,
-                         show_median: show_median,
-                         selected_median_color: selected_median_color,
-                         unselect_median_color: unselect_median_color,
-                         selected: selected,
-                         screen_size: proxy.size
-                )
-                    .padding(.top, 5)
+                ZStack(alignment: .center){
+                    // MARK: Lines appear when show lines equal to true
+                    if x_axis_color != nil {
+                        VStack(spacing: 0){
+                            ForEach(getGraphLines(),id: \.self){line in
+                                HStack {
+                                    Text("\(Int(line))")
+                                        .font(.system(.caption, design: .rounded))
+                                        .foregroundColor(x_name_color)
+                                        .frame(height: 20)
+                                    
+                                    Rectangle()
+                                        .fill(x_axis_color!)
+                                        .frame(height: 1)
+                                }
+                                .frame(maxHeight: .infinity,alignment: .bottom)
+                                .offset(y: -15)
+                            }
+                        }
+                    }
+                    
+                    
+                    // MARK: Bar Graph
+                    HStack{
+                        ForEach(Array(zip(chart_data.indices, chart_data)), id: \.0){ index, each_data in
+                            VStack(spacing: 0){
+                                VStack(spacing: 5){
+                                    AnimatedBarGraph(selected: $selected,
+                                                     each_graph_data: each_data,
+                                                     index: index,
+                                                     graph_color: chart_gradient,
+                                                     is_selectable: is_selectable,
+                                                     show_values: (x_name_color != nil))
+                                }
+                                .frame(height: each_data.value == 0 ? 0 : getBarHeight(point: each_data.value, size: proxy.size))
+                                
+                                // MARK: Bottom Text
+                                Text(each_data.name)
+                                    .font(.system(.caption2, design: .rounded))
+                                    .foregroundColor(x_name_color ?? Color(.white).opacity(0))
+                                    .frame(height: 25,alignment: .bottom)
+                            }
+                            .frame(maxWidth: 30, maxHeight: .infinity, alignment: .bottom)
+                            
+                            // Spacer arguments
+                            if chart_data.count != index+1 {
+                                Spacer(minLength: 3)
+                            }
+                        }
+                    }
+                    .padding(.leading, (x_axis_color != nil) ? 30 : 0)
+                    
+                    // MARK: MEDIAN DATA
+                    if show_median {
+                        let median = getMax()/2
+                        VStack (alignment: .leading, spacing: 0){
+                            Text(String(format: "%.0f", median))
+                                .font(.system(.callout, design: .rounded))
+                                .foregroundColor(selected == 0 ? unselect_median_color : selected_median_color.opacity(0.8))
+                                .fontWeight(.semibold)
+                            
+                            RoundedRectangle(cornerRadius: 5)
+                                .foregroundColor(selected == 0 ? unselect_median_color : selected_median_color.opacity(0.8))
+                                .frame(height: 5)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(.leading, (x_axis_color != nil) ? 30 : 0)
+                        .padding(.bottom, 30)
+                    }
+                }
             }
         }
         .padding(20)
@@ -133,7 +191,43 @@ public struct ColumnChartView: View {
                 .foregroundColor(background)
                 .cornerRadius(10)
         }
+    }
+    
+    func getBarHeight(point: CGFloat,size: CGSize)->CGFloat{
         
+        let max = getMax()
+        
+        let height = (point / max) * (size.height - 37)
+        
+        return height
+    }
+    
+    // MARK: Getting Sample Graph Lines based on max Value...
+    func getGraphLines()->[CGFloat]{
+        
+        let max = getMax()
+        
+        var lines: [CGFloat] = []
+        
+        lines.append(max)
+        
+        for index in 1...5{
+            
+            // dividing the max by 4 and iterating as index for graph lines...
+            let progress = max / 5
+            
+            lines.append(max - (progress * CGFloat(index)))
+        }
+        
+        return lines
+    }
+    
+    // MARK: Getting Max....
+    func getMax()->CGFloat{
+        let max = chart_data.max { first, scnd in
+            return scnd.value > first.value
+        }?.value ?? 0
+        return max
     }
 }
 
@@ -175,6 +269,7 @@ struct BarGraph: View {
                     }
                 }
             }
+            
             
             // MARK: Bar Graph
             HStack{
