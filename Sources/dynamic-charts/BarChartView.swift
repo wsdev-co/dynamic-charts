9a510f32fb2cc7fde72ec98d73a74f3a0bc86050
Inagamjanov <umarjoninogomjonov9@gmail.com>
2023-01-19 16:39:35 +0500

    wsu

--- Sources/dynamic-charts/BarChartView.swift
+++ Sources/dynamic-charts/BarChartView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import XCTest
 
 
 // MARK: SCHEME
--- Tests/dynamic-chartsTests/dynamic_chartsTests.swift
+++ Tests/dynamic-chartsTests/dynamic_chartsTests.swift
@@ -6,5 +6,6 @@ final class dynamic_chartsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+//        XCTAssertEqual(dynamic_charts, "Hello, World!")
     }
 }
